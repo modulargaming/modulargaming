933bfddbb39b26e39c7c11e39a9e2e762c7b9149
Hinton <hinton.oscar@gmail.com>
2012-12-10 20:23:56 +0100

    Changed Welcome to use Kostache.

--- application/classes/Controller/Welcome.php
+++ application/classes/Controller/Welcome.php
@@ -4,7 +4,8 @@ class Controller_Welcome extends Controller {
 
 	public function action_index()
 	{
-		$this->response->body('hello, world!');
+		$renderer = Kostache::factory();
+		$this->response->body($renderer->render(new View_Welcome));
 	}
 
 } // End Welcome
