9d1fa736da97b606e49ecbc0f31fa71f0ce34abd
Curtis Delicata <curtis@modulargaming.com>
2012-12-11 19:34:00 +0100

    Add Models, View and Controller

---
+++ application/classes/View/Account/Create.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class View_Account_Create
+{
+        public $title = 'Account Create';
+}
+
---
+++ application/classes/View/Account/Login.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') or die('No direct script access.');
+
+class View_Account_Login
+{
+        public $title = 'Account - Login';
+}
+
