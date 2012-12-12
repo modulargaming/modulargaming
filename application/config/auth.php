534be6fd735911b633ec1b8944cee4caac865a22
Hinton <hinton.oscar@gmail.com>
2012-12-12 14:10:30 +0100

    Cleaned up the config files.
    Indented the config files.
    Removed modulargaming, encrypt config files.

--- application/config/auth.php
+++ application/config/auth.php
@@ -1,10 +1,10 @@
-<?php defined('SYSPATH') or die('No direct access allowed.');
+<?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array(
-'driver' => 'ORM',
-'hash_method' => 'sha256',
-'hash_key' => 'change me',
-'lifetime' => 0,
-'session_type' => Session::$default,
-'session_key' => 'auth_user',
+	'driver'       => 'ORM',
+	'hash_method'  => 'sha256',
+	'hash_key'     => 'change me',
+	'lifetime'     => 1209600,
+	'session_type' => Session::$default,
+	'session_key'  => 'auth_user',
 );
