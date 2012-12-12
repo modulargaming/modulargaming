273edb24eebf2cae52ac41efaa369744a3196c6d
Curtis Delicata <curtis@modulargaming.com>
2012-12-12 11:58:58 +0100

    Remove account files

--- application/classes/View/Account/Create.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class View_Account_Create
-{
-        public $title = 'Account Create';
-}
-
--- application/classes/View/Account/Login.php
+++
@@ -1,7 +0,0 @@
-<?php defined('SYSPATH') or die('No direct script access.');
-
-class View_Account_Login
-{
-        public $title = 'Account - Login';
-}
-
