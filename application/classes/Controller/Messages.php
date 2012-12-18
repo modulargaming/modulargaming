7727c1779a4e856ce611077c046548ec97f3b295
Hinton <hinton.oscar@gmail.com>
2012-12-18 14:44:24 +0100

    Cleaned up messages controller and view.

--- application/classes/Controller/Messages.php
+++ application/classes/Controller/Messages.php
@@ -5,33 +5,13 @@ class Controller_Messages extends Controller_Frontend {
 	public function action_index()
 	{
 		$this->view = new View_Messages_Index;
-		$message = ORM::factory('Message');
-		$message->where('to_id', '=', $this->user->id);
-		$this->view->total = $message->count_all();
 
-	}
+		// TODO: Add pagination
+		$messages = ORM::factory('Message')
+			->where('to_id', '=', $this->user->id)
+			->find_all();
 
-
-	public function list_messages()
-	{
-		$message = ORM::factory('Message');
-		$message->where('to_id', '=', $this->user->id);
-		$messages = $message->find_all();
-
-		$list = array();
-
-		foreach ($messages as $message)
-		{
-			$list[] = array(
-				'href'     => Route::url('messages', array('id' => $message->id)),
-				'subject' => $message->subject,
-				'text' => $message->text,
-				'from_id' => $message->from_id,
-				'folder_id' => $message->folder_id,
-			);
-		}
-
-		return $list;
+		$this->view->messages = $messages;
 	}
 
 	public function action_create()
--- application/classes/View/Messages/Index.php
+++ application/classes/View/Messages/Index.php
@@ -3,4 +3,28 @@
 class View_Messages_Index extends View_Base
 {
 	public $title = 'Messages';
+
+	public function messages()
+	{
+		$messages = array();
+
+		foreach ($this->messages as $message)
+		{
+			$messages[] = array(
+				//'href'     => Route::url('messages', array('id' => $message->id)),
+				'subject' => $message->subject,
+				'text' => $message->text,
+				'from_id' => $message->from_id,
+				'folder_id' => $message->folder_id,
+			);
+		}
+
+		return $messages;
+	}
+
+	public function total()
+	{
+		return count($this->messages);
+	}
+
 }
