a3ca7c78e604ec61de302fff6300cc4fb5fab1ca
Hinton <hinton.oscar@gmail.com>
2012-12-18 14:32:44 +0100

    Fixed indention in Messages controller.

--- application/classes/Controller/Messages.php
+++ application/classes/Controller/Messages.php
@@ -7,7 +7,7 @@ class Controller_Messages extends Controller_Frontend {
 		$this->view = new View_Messages_Index;
 		$message = ORM::factory('Message');
 		$message->where('to_id', '=', $this->user->id);
-   		$this->view->total = $message->count_all();
+		$this->view->total = $message->count_all();
 
 	}
 
@@ -18,50 +18,50 @@ class Controller_Messages extends Controller_Frontend {
 		$message->where('to_id', '=', $this->user->id);
 		$messages = $message->find_all();
 
-    		$list = array();
+		$list = array();
 
-    		foreach ($messages as $message)
-    		{
-        		$list[] = array(
-            		'href'     => Route::url('messages', array('id' => $message->id)),
-            		'subject' => $message->subject,
-			'text' => $message->text,
-			'from_id' => $message->from_id,
-			'folder_id' => $message->folder_id,
-        		);
-    		}
+		foreach ($messages as $message)
+		{
+			$list[] = array(
+				'href'     => Route::url('messages', array('id' => $message->id)),
+				'subject' => $message->subject,
+				'text' => $message->text,
+				'from_id' => $message->from_id,
+				'folder_id' => $message->folder_id,
+			);
+		}
 
-   	 	return $list;
+		return $list;
 	}
 
-        public function action_create()
-        {
-                if ($_POST)
-                {
-                        try
-                        {
+	public function action_create()
+	{
+		if ($_POST)
+		{
+			try
+			{
 
 				$array = $this->request->post();
 				$array['from_id'] = $this->user->id;
-                                $message = ORM::Factory('Message')
-                                        ->create_message($array, array(
-                                                'to_id',
-                                                'subject',
-                                                'text',
+				$message = ORM::Factory('Message')
+					->create_message($array, array(
+						'to_id',
+						'subject',
+						'text',
 						'from_id',
-                                        ));
+					));
 
-                                $this->redirect('messages');
-                        }
-                        catch (ORM_Validation_Exception $e)
-                        {
-                                Hint::error($e->errors('models'));
-                        }
+				$this->redirect('messages');
+			}
+			catch (ORM_Validation_Exception $e)
+			{
+				Hint::error($e->errors('models'));
+			}
 
-                }
+		}
 
-                $this->view = new View_Messages_Create;
-        }
+		$this->view = new View_Messages_Create;
+	}
 
 
 } // End Messages
