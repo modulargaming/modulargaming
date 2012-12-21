a5442f8f75f65c20863476d2af25cf03f539adb5
Curtis Delicata <curtis@modulargaming.com>
2012-12-21 15:13:03 +0000

    Rename d-toolbar to debug-toolbar

--- application/bootstrap.php
+++ application/bootstrap.php
@@ -134,7 +134,7 @@ Kohana::modules(array(
 	'orm'        => MODPATH.'orm',        // Object Relationship Mapping
 	'unittest'   => MODPATH.'unittest',   // Unit testing
 	'userguide'  => MODPATH.'userguide',  // User guide and API documentation
-	'd-toolbar'  => MODPATH.'debug-toolbar',
+	'debug-toolbar'  => MODPATH.'debug-toolbar',
 	));
 
 /**
