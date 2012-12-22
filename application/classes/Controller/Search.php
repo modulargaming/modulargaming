b9b15b5169e8b30a0744fae982a55d27192c79e1
Hinton <hinton.oscar@gmail.com>
2012-12-22 02:28:32 +0100

    Added search controller and views #8.

---
+++ application/classes/Controller/Search.php
@@ -0,0 +1,33 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Controller_Search extends Controller_Frontend {
+
+	public function action_index()
+	{
+		$this->view = new View_Search;
+
+		if (isset($_GET['query']))
+		{
+			$query = $_GET['query'];
+
+			if (strlen($query) < 3)
+			{
+				Hint::error('Query needs to be longer than 3 characters');
+				return;
+			}
+
+			$sql_query = '%' . $query . '%';
+
+			$users = ORM::Factory('User')
+				->where('username', 'LIKE', $sql_query)
+				->find_all();
+
+			// TODO: Decide how to display the search results.
+
+			$this->view->query = $query;
+		}
+
+	}
+
+
+}
---
+++ application/classes/View/Search.php
@@ -0,0 +1,7 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class View_Search extends View_Base {
+
+	public $title = 'Search';
+
+}
