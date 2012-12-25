6d1c410d738badda722af9267fd574938527aedf
Curtis Delicata <curtis@modulargaming.com>
2012-12-25 12:41:29 +0000

    Add profile fields to user/view

--- application/classes/View/User/Profile.php
+++ application/classes/View/User/Profile.php
@@ -9,4 +9,14 @@ class View_User_Profile extends View_Base {
 		return $this->profile_user['username'] . '\'s Profile';
 	}
 
+        public function profile_user()
+        {
+                $profile_user = $this->profile_user;
+                $profile_user['last_login'] = Date::format($profile_user['last_login']);
+                $profile_user['created'] = Date::format($profile_user['created']);
+
+                return $profile_user;
+        }
+
+
 }
