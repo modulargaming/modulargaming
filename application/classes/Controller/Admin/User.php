46dd5f8bc6f9baab48ebc2ff7ce32509420792bd
Hinton <hinton.oscar@gmail.com>
2012-12-26 01:40:09 +0100

    Added action_view to edit user, admin panel.

--- application/classes/Controller/Admin/User.php
+++ application/classes/Controller/Admin/User.php
@@ -4,11 +4,25 @@ class Controller_Admin_User extends Controller_Admin {
 
 	public function action_index()
 	{
-		$users = ORM::Factory('User')
+		$users = ORM::factory('User')
 			->find_all();
 
 		$this->view = new View_Admin_User_Index;
 		$this->view->users = $users->as_array();
+	}
+
+	public function action_view()
+	{
+		$id = $this->request->param('id');
+
+		$user = ORM::factory('User', $id);
+
+		if ( ! $user->loaded())
+		{
+			throw HTTP_Exception::factory('404', 'No such user');
+		}
+
+
 	}
 
 }
