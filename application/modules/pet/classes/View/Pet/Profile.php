8ed28d606b91cdad03c97a1615d51e3ec0b31608
Curtis Delicata <curtis@modulargaming.com>
2012-12-30 11:56:36 +0000

    Add created time on pet profile

--- application/modules/pet/classes/View/Pet/Profile.php
+++ application/modules/pet/classes/View/Pet/Profile.php
@@ -3,4 +3,14 @@
 class View_Pet_Profile extends View_Base {
 
 	public $title = 'Pet name';
+
+        public function pet()
+        {
+                $pet = $this->pet;
+                $pet->created = Date::format($pet->created);
+
+                return $pet;
+        }
+
+
 }
