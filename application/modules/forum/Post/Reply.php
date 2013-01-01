98b92020a76b066021c4b70cd8f4268315727c12
Curtis Delicata <curtis@modulargaming.com>
2013-01-01 10:49:25 +0000

    Remove files added by mistake @Lewis

--- application/modules/forum/Post/Reply.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class View_Forum_Post_Reply extends View_Base {
-
-	public $title = 'Reply';
-	public $category;
-
-}
--- application/modules/forum/Topic/Create.php
+++
@@ -1,8 +0,0 @@
-<?php defined('SYSPATH') OR die('No direct script access.');
-
-class View_Forum_Topic_Create extends View_Base {
-
-	public $title = 'Create Topic';
-	public $category;
-
-}
