0ba658eb9788d305732ba5726db96e2fff68d630
Curtis Delicata <curtis@modulargaming.com>
2013-01-03 11:00:57 +0000

    HTML Purifier on input

--- application/modules/forum/classes/View/Forum/Topic/Create.php
+++ application/modules/forum/classes/View/Forum/Topic/Create.php
@@ -5,4 +5,10 @@ class View_Forum_Topic_Create extends View_Base {
 	public $title = 'Create Topic';
 	public $category;
 
+	public function post()
+	{
+		$post = $this->post->as_array();
+		$post['content'] = Security::xss_clean($post['content']);
+		return $post;
+	}
 }
