ed1493242288ccc16bf804094891a3d28050d84e
Hinton <hinton.oscar@gmail.com>
2013-01-03 14:47:33 +0100

    Moved js libs to libs folder. Added a script.js file for custom javascript. Added textarea-basic without font-styles and lists.

---
+++ assets/js/script.js
@@ -0,0 +1,11 @@
+/**
+ * Modular Gaming js file.
+ */
+
+$(document).ready(function() {
+	$('.textarea').wysihtml5();
+	$('.textarea-basic').wysihtml5({
+		'font-styles': false,
+		'lists': false
+	});
+});
