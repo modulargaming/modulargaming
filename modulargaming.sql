0279815c0d845f32a4452f1abb224e7919f97233
Curtis Delicata <curtis@modulargaming.com>
2013-01-07 11:25:16 +0000

    Remove default from user_titles

--- modulargaming.sql
+++ modulargaming.sql
@@ -69,7 +69,7 @@ ALTER TABLE `user_tokens`
 
 
 CREATE TABLE IF NOT EXISTS `user_titles` (
-  `id` int(11) NOT NULL DEFAULT '1',
+  `id` int(11) NOT NULL,
   `title` varchar(55) NOT NULL,
   `description` varchar(255) NOT NULL,
   PRIMARY KEY (`id`)
