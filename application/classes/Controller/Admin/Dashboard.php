2913ecbe4e605edcce8b02bf4b434f2bc8e135c6
Curtis Delicata <curtis@modulargaming.com>
2013-01-08 13:54:40 +0000

    Admin dashboard policy

--- application/classes/Controller/Admin/Dashboard.php
+++ application/classes/Controller/Admin/Dashboard.php
@@ -4,6 +4,11 @@ class Controller_Admin_Dashboard extends Controller_Admin {
 
 	public function action_index()
 	{
+
+		if ( ! $this->user->can('Admin_Dashboard_Index') )
+		{
+			throw HTTP_Exception::factory('403', 'Permission denied to access admin dashboard index ');
+		}
 		$this->view = new View_Admin_Dashboard;
 	}
 
