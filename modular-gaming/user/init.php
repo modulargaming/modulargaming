d1f8dbebc12bd941055d783b253872a03964ba4b
Curtis Delicata <curtis@modulargaming.com>
2013-01-10 20:48:07 +0000

    Fixed user view

--- modular-gaming/user/init.php
+++ modular-gaming/user/init.php
@@ -1,8 +1,15 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
 Route::set('user', 'user(/<controller>(/<action>(/<id>)))')
-->defaults(array(
-'directory'  => 'user',
-'controller' => 'dashboard',
-'action'     => 'index',
+	->defaults(array(
+		'directory'  => 'user',
+		'controller' => 'dashboard',
+		'action'     => 'index',
 ));
+
+Route::set('user', 'user/view/(<id>)', array('id' => '[0-9]+'))
+	->defaults(array(
+		'directory'  => 'user',
+		'controller' => 'view',
+		'action'     => 'index',
+	));
