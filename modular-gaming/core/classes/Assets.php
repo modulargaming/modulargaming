19eaf9e6f36a54fd643717081c91a66584045f6b
Kerstens Maxim <admin@happydemon.org>
2013-01-14 16:50:27 +0100

    Assets variable naming correction

--- modular-gaming/core/classes/Assets.php
+++ modular-gaming/core/classes/Assets.php
@@ -4,20 +4,20 @@ class Assets {
 	static $_collection = array('head' => array(), 'body' => array());
 	static $_files = array('css', 'js');
 	
-	static public function js($name, $file, $location='body', $possition='end', $relative=null) {
-		self::add('js', $name, $file, $location, $possition, $relative);
+	static public function js($name, $file, $location='body', $position ='end', $relative=null) {
+		self::add('js', $name, $file, $location, $position, $relative);
 	}
 	
-	static public function css($name, $file, $location='head', $possition='end', $relative=null) {
-		self::add('css', $name, $file, $location, $possition, $relative);
+	static public function css($name, $file, $location='head', $position='end', $relative=null) {
+		self::add('css', $name, $file, $location, $position, $relative);
 	}
 	
-	static public function add($type, $name, $file, $location, $possition='end', $relative=null) {
+	static public function add($type, $name, $file, $location, $position='end', $relative=null) {
 		//assign the file
 		self::$_files[$type][$name] = $file;
 		
 		//place the file
-		switch($possition) {
+		switch($position) {
 			default:
 			case'end':
 				self::$_collection[$location][$type][] =  $name;
@@ -30,7 +30,7 @@ class Assets {
 			case 'after':
 				$insertion = array_search($relative, self::$_collection[$location][$type]);
 				
-				if($possition == 'after')
+				if($position == 'after')
 					$insertion++;
 		
 				$before = array_slice(self::$_collection[$location][$type], 0, $insertion, true);
