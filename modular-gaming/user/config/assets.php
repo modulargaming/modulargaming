96905729503380be3562928c8769e6ba5b9be63c
Hinton <hinton.oscar@gmail.com>
2013-01-15 13:43:48 +0100

    Fixed admin user.js.

---
+++ modular-gaming/user/config/assets.php
@@ -0,0 +1,14 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+	'admin_user' => array(
+		'body' => array(
+			'js' => array(
+				array(
+					'name' => 'user',
+					'file' => 'admin/user.js'
+				),
+			),
+		),
+	)
+);
