42c713156ca07f6c92dfbbf24bab537bbee0b8b1
Curtis Delicata <curtis@modulargaming.com>
2013-01-18 10:51:37 +0000

    Add missing pet models

---
+++ modular-gaming/pet/classes/Model/Pet/Specie.php
@@ -0,0 +1,20 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+class Model_Pet_Specie extends ORM {
+
+
+	protected $_has_many = array(
+		'pets' => array(
+			'model' => 'User_Pet',
+			'foreign_key' => 'specie_id',
+		),
+	);
+
+	static public function specie_exists($id)
+	{
+		$specie = ORM::factory('Pet_Specie', $id);
+
+		return $specie->loaded();
+	}
+
+}
