0285bdcb1c4cb5ea05554e172c7731f86d6e9080
Curtis Delicata <curtis@modulargaming.com>
2013-01-19 14:53:55 +0000

    Add route for messages

--- modular-gaming/message/init.php
+++ modular-gaming/message/init.php
@@ -1,5 +1,13 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
+Route::set('messages', 'messages')
+  ->defaults(array(  	
+    'directory'  => 'message',	  	
+    'controller' => 'index',	  	
+    'action'     => 'index',	  	
+  ));
+
+
 Route::set('message.view', 'message/view(/<id>)', array('id' => '[0-9]+'))
   ->defaults(array(  	
     'directory'  => 'message',	  	
