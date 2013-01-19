a883793c9a1df4c822df23ead6801b20b7905d23
Curtis Delicata <curtis@modulargaming.com>
2013-01-19 14:25:47 +0000

    Improve messaging view and template #28

--- modular-gaming/user/classes/View/Messages/View.php
+++ modular-gaming/user/classes/View/Messages/View.php
@@ -11,9 +11,23 @@ class View_Messages_View extends Abstract_View {
 
         public function message()
         {
-                $message = $this->message;
-                $message->created = Date::format($message->created);
+		$message[] = array(
+				'id' => $this->message->id,
+				'title' => $this->message->subject,
+				'content' => $this->message->content, // Escaped properly at create now
+				'created' =>  Date::format($this->message->created),
+				'sender' => array(
+					'avatar' => $this->message->sender->avatar,
+					'username'  => $this->message->sender->username,
+					'title'  => $this->message->sender->title->title,
+					'signature' => $this->message->sender->signature,
+					'created' => Date::format($this->message->created),
+					'href'      => Route::url('user.view', array(
+						'id'     => $this->message->sender->id,
+					)),
+				),
 
+				);
                 return $message;
         }
 
