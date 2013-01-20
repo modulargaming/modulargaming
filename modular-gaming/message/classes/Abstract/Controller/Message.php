78c2dfe1be7281fb09d3038161e23424fd8219c2
Curtis Delicata <curtis@modulargaming.com>
2013-01-20 10:54:53 +0000

    Messages breadcrumb #28

--- modular-gaming/message/classes/Abstract/Controller/Message.php
+++ modular-gaming/message/classes/Abstract/Controller/Message.php
@@ -5,4 +5,11 @@ class Abstract_Controller_Message extends Abstract_Controller_Frontend {
 
 	protected $protected = TRUE;
 
+
+	public function before()
+	{
+		parent::before();
+
+		Breadcrumb::add('Messages', Route::url('messages'));
+	}
 }
