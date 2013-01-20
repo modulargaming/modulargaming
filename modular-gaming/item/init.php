177a4c8f3a4f3dd94f682b27e139679d23e14750
Kerstens Maxim <admin@happydemon.org>
2013-01-20 20:46:56 +0100

    forgot to update item's init.php

--- modular-gaming/item/init.php
+++ modular-gaming/item/init.php
@@ -40,21 +40,21 @@ Route::set('item.admin.type.search', 'admin/item/types/search')
 ));
 Route::set('item.admin.type.save', 'admin/item/types/save')
 	->defaults(array(
-		'directory'  => 'admin',
-		'controller' => 'item',
-		'action'     => 'save_type',
+		'directory'  => 'admin/item',
+		'controller' => 'types',
+		'action'     => 'save',
 ));
 Route::set('item.admin.type.retrieve', 'admin/item/types/retrieve')
 	->defaults(array(
-		'directory'  => 'admin',
-		'controller' => 'item',
-		'action'     => 'retrieve_type',
+		'directory'  => 'admin/item',
+		'controller' => 'types',
+		'action'     => 'retrieve',
 ));
 Route::set('item.admin.type.delete', 'admin/item/types/delete')
 	->defaults(array(
-		'directory'  => 'admin',
-		'controller' => 'item',
-		'action'     => 'delete_type',
+		'directory'  => 'admin/item',
+		'controller' => 'types',
+		'action'     => 'delete',
 ));
 Route::set('item.admin.recipe.search', 'admin/item/recipes/search')
 	->defaults(array(
@@ -64,33 +64,33 @@ Route::set('item.admin.recipe.search', 'admin/item/recipes/search')
 ));
 Route::set('item.admin.recipe.retrieve', 'admin/item/recipes/retrieve')
 	->defaults(array(
-		'directory'  => 'admin',
-		'controller' => 'item',
-		'action'     => 'recipe_retrieve',
+		'directory'  => 'admin/item',
+		'controller' => 'recipes',
+		'action'     => 'retrieve',
 ));
 Route::set('item.admin.recipe.save', 'admin/item/recipes/save')
 	->defaults(array(
-		'directory'  => 'admin',
-		'controller' => 'item',
-		'action'     => 'recipe_save',
+		'directory'  => 'admin/item',
+		'controller' => 'recipes',
+		'action'     => 'save',
 ));
 Route::set('item.admin.recipe.delete', 'admin/item/recipes/delete')
 	->defaults(array(
-		'directory'  => 'admin',
-		'controller' => 'item',
-		'action'     => 'recipe_delete',
+		'directory'  => 'admin/item',
+		'controller' => 'recipes',
+		'action'     => 'delete',
 ));
 Route::set('item.admin.recipe.index', 'admin/item/recipes(/<page>)', array('page' => '[0-9]+'))
 	->defaults(array(
-		'directory'  => 'admin',
-		'controller' => 'item',
-		'action'     => 'recipes',
+		'directory'  => 'admin/item',
+		'controller' => 'recipes',
+		'action'     => 'index',
 ));
 Route::set('item.admin.types.index', 'admin/item/types(/<page>)', array('page' => '[0-9]+'))
 	->defaults(array(
-		'directory'  => 'admin',
-		'controller' => 'item',
-		'action'     => 'types',
+		'directory'  => 'admin/item',
+		'controller' => 'types',
+		'action'     => 'index',
 ));
 Route::set('item.admin.list.index', 'admin/item(/<page>)', array('page' => '[0-9]+'))
 	->defaults(array(
