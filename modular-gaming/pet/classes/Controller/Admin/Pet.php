a7747107293606ee6aa14a7f1a75b7eb53accb35
Curtis Delicata <curtis@modulargaming.com>
2013-01-22 10:11:34 +0000

    Pet routes

--- modular-gaming/pet/classes/Controller/Admin/Pet.php
+++ modular-gaming/pet/classes/Controller/Admin/Pet.php
@@ -4,14 +4,16 @@ class Controller_Admin_Pet extends Abstract_Controller_Admin {
 	
 	public function action_index()
 	{
+		$id = $this->request->param('id');
 
 		if ( ! $this->user->can('Admin_Pet_Index') )
 		{
 			throw HTTP_Exception::factory('403', 'Permission denied to view admin pets index');
 		}
 		
-		$this->redirect(Route::get('pet.admin.specie')->uri(array('id' => $this->user->id)));
-
+//		$this->redirect(Route::get('pet.admin.specie'));
+//		$this->redirect(Route::get('pet.admin.specie')->uri(array('id' => $this->user->id)));
+		$this->redirect(Route::get('pet.admin.specie')->uri(array('id' => $id)));
 		$this->view = new View_Admin_Pet_Index;
 	}
 
--- modular-gaming/pet/init.php
+++ modular-gaming/pet/init.php
@@ -28,19 +28,20 @@ Route::set('pet', 'pet/<name>', array('name' => '[a-zA-Z0-9-_]+'))
 		'action'     => 'Index',
 	));
 
+//Route::set('pet.admin.index', 'admin/pet(/<action>(/<id>))', array('id' => '[0-9]+'))
 Route::set('pet.admin.index', 'admin/pet')
 	->defaults(array(
 		'directory'  => 'Admin',
 		'controller' => 'Pet',
 		'action'     => 'Index',
 ));
-Route::set('pet.admin.colour', 'admin/pet/colour')
+Route::set('pet.admin.colour', 'admin/pet/colour(/<action>(/<id>))', array('id' => '[0-9]+'))
 	->defaults(array(
 		'directory'  => 'Admin/Pet',
 		'controller' => 'Colour',
 		'action'     => 'Index',
 ));
-Route::set('pet.admin.specie', 'admin/pet/specie')
+Route::set('pet.admin.specie', 'admin/pet/specie(/<action>(/<id>))', array('id' => '[0-9]+'))
 	->defaults(array(
 		'directory'  => 'Admin/Pet',
 		'controller' => 'Specie',
