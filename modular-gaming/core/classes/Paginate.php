944c4b5017a013fb05b445a9d78a540dcb98a2cf
Hinton <hinton.oscar@gmail.com>
2013-01-25 16:32:59 +0100

    Renamed cfg to config (Paginate).

--- modular-gaming/core/classes/Paginate.php
+++ modular-gaming/core/classes/Paginate.php
@@ -2,7 +2,7 @@
 
 abstract class Paginate extends Kohana_Paginate {
 
-	public $cfg = null;
+	private $config = null;
 	public $current_page = false;
 	
 	public static function factory($object, $config = 'default', $driver = NULL)
@@ -12,7 +12,7 @@ abstract class Paginate extends Kohana_Paginate {
 		$object->set_config($config);
 		
 		//get the current page
-		$page = Request::initial()->param($object->cfg['param']);
+		$page = Request::initial()->param($object->config['param']);
 		
 		if ( ! Valid::digit($page))
 		{
@@ -22,7 +22,7 @@ abstract class Paginate extends Kohana_Paginate {
 		$object->current_page = $page;
 		
 		//limit the pagination results
-		$object->limit(($page - 1) * $object->cfg['total_items'], $object->cfg['total_items']);
+		$object->limit(($page - 1) * $object->config['total_items'], $object->config['total_items']);
 		
 		return $object;
 	}
@@ -31,34 +31,34 @@ abstract class Paginate extends Kohana_Paginate {
 	{
 		if ($config == 'default')
 		{
-			$this->cfg = Kohana::$config->load('pagination')->as_array();
+			$this->config = Kohana::$config->load('pagination')->as_array();
 		}
 		elseif (is_array($config))
 		{
 			if (count($config) < 4)
 			{
-				$this->cfg = array_merge(Kohana::$config->load('pagination')->as_array(), $config);
+				$this->config = array_merge(Kohana::$config->load('pagination')->as_array(), $config);
 			}
 			else
 			{
-				$this->cfg = $config;
+				$this->config = $config;
 			}
 		}
 		else
 		{
-			$this->cfg = Kohana::$config->load($config)->as_array();
+			$this->config = Kohana::$config->load($config)->as_array();
 		}
 	}
 	
 	public function pages()
 	{
-		return ceil($this->_count_total / $this->cfg['total_items']);
+		return ceil($this->_count_total / $this->config['total_items']);
 	}
 
 	public function kostache()
 	{
 		//if we only have 1 page and autohide is on return nothing
-		if ($this->_count == $this->_count_total AND $this->cfg['auto_hide'] == TRUE)
+		if ($this->_count == $this->_count_total AND $this->config['auto_hide'] == TRUE)
 		{
 			return false;
 		}
@@ -71,7 +71,7 @@ abstract class Paginate extends Kohana_Paginate {
 			{
 				$active = ($this->current_page == $i);
 				
-				$link = URL::site(Request::initial()->route()->uri(array($this->cfg['param'] => $i)));
+				$link = URL::site(Request::initial()->route()->uri(array($this->config['param'] => $i)));
 				$pages[] = array('num' => $i, 'link' => $link, 'active' => $active);
 			}
 			
