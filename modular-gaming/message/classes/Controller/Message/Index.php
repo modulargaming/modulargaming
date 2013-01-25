590eaa94547c36cd215a1177fe52cda147c4d3b2
Curtis Delicata <curtis@modulargaming.com>
2013-01-25 17:24:57 +0000

    Message pagination

--- modular-gaming/message/classes/Controller/Message/Index.php
+++ modular-gaming/message/classes/Controller/Message/Index.php
@@ -10,10 +10,13 @@ class Controller_Message_Index extends Abstract_Controller_Message {
 		// TODO: Add pagination
 		$messages = ORM::factory('Message')
 			->where('receiver_id', '=', $this->user->id)
-			->order_by('created', 'DESC')
-			->find_all();
+			->order_by('created', 'DESC');
 
-		$this->view->messages = $messages;
+		$paginate = Paginate::factory($messages)
+			->execute();
+
+		$this->view->pagination = $paginate->render();
+		$this->view->messages = $paginate->result();
 	}
 
 
