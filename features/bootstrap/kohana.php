c8c7982f3d45bed00a06d0d25c4d13b145fe8c59
Hinton <hinton.oscar@gmail.com>
2013-01-26 19:31:38 +0100

    Fixed behat kohana bootstrap.

--- features/bootstrap/kohana.php
+++ features/bootstrap/kohana.php
@@ -4,6 +4,7 @@
 defined('APPPATH') ?: define('APPPATH', 'application/');
 defined('MODPATH') ?: define('MODPATH', 'modules/');
 defined('SYSPATH') ?: define('SYSPATH', 'system/');
+defined('MGPATH')  ?: define('MGPATH', 'modular-gaming/');
 defined('EXT') ?: define('EXT', '.php');
 
 require_once APPPATH.'bootstrap.php';
