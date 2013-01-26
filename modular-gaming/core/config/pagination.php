4ab562439a09cb1c2bb494c0aa5d502051cf68f9
Curtis Delicata <curtis@modulargaming.com>
2013-01-26 09:52:27 +0000

    Set pagination max to 20

--- modular-gaming/core/config/pagination.php
+++ modular-gaming/core/config/pagination.php
@@ -1,7 +1,7 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array (
-	'total_items' => 2,
+	'total_items' => 20,
 	'param' => 'page',
 	'view' => 'Pagination',
 	'auto_hide' => TRUE,
