e2bb3d122d6a293260e2e372fb377013038a7abe
Kerstens Maxim <admin@happydemon.org>
2013-01-29 13:51:08 +0100

    pet admin routes
    forgot to add these in the last commit

--- modular-gaming/pet/init.php
+++ modular-gaming/pet/init.php
@@ -28,25 +28,68 @@ Route::set('pet', 'pet/<name>', array('name' => '[a-zA-Z0-9-_]+'))
 		'action'     => 'index',
 	));
 
-//Route::set('pet.admin.index', 'admin/pet(/<action>(/<id>))', array('id' => '[0-9]+'))
+
 Route::set('pet.admin.index', 'admin/pet')
 	->defaults(array(
 		'directory'  => 'Admin',
 		'controller' => 'Pet',
 		'action'     => 'index',
 ));
-Route::set('pet.admin.colour', 'admin/pet/colour(/<action>(/<id>))', array('id' => '[0-9]+'))
+Route::set('pet.admin.search', 'admin/pet/search')
+	->defaults(array(
+		'directory'  => 'Admin',
+		'controller' => 'Pet',
+		'action'     => 'search',
+));
+Route::set('pet.admin.specie.retrieve', 'admin/pet/specie/retrieve')
 	->defaults(array(
 		'directory'  => 'Admin/Pet',
-		'controller' => 'Colour',
-		'action'     => 'index',
+		'controller' => 'Specie',
+		'action'     => 'retrieve',
 ));
-Route::set('pet.admin.specie', 'admin/pet/specie(/<action>(/<id>))', array('id' => '[0-9]+'))
+Route::set('pet.admin.specie.delete', 'admin/pet/specie/delete')
+	->defaults(array(
+		'directory'  => 'Admin/Pet',
+		'controller' => 'Specie',
+		'action'     => 'delete',
+));
+Route::set('pet.admin.specie.save', 'admin/pet/specie/save')
+	->defaults(array(
+		'directory'  => 'Admin/Pet',
+		'controller' => 'Specie',
+		'action'     => 'save',
+));
+Route::set('pet.admin.specie.index', 'admin/pet/specie')
 	->defaults(array(
 		'directory'  => 'Admin/Pet',
 		'controller' => 'Specie',
 		'action'     => 'index',
 ));
+Route::set('pet.admin.colour.retrieve', 'admin/pet/colour/retrieve')
+	->defaults(array(
+		'directory'  => 'Admin/Pet',
+		'controller' => 'Colour',
+		'action'     => 'retrieve',
+));
+Route::set('pet.admin.colour.delete', 'admin/pet/colour/delete')
+	->defaults(array(
+		'directory'  => 'Admin/Pet',
+		'controller' => 'Colour',
+		'action'     => 'delete',
+));
+Route::set('pet.admin.colour.save', 'admin/pet/colour/save')
+	->defaults(array(
+		'directory'  => 'Admin/Pet',
+		'controller' => 'Colour',
+		'action'     => 'save',
+));
+Route::set('pet.admin.colour.index', 'admin/pet/colour')
+	->defaults(array(
+		'directory'  => 'Admin/Pet',
+		'controller' => 'Colour',
+		'action'     => 'index',
+));
+	
 //Add link to manage forums in admin
 Event::listen('admin.nav_list', function(){
 	return array(
