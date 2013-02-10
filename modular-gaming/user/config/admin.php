e938914baab0efe63422b61189e0969ecd4baf23
Kerstens Maxim <admin@happydemon.org>
2013-02-10 23:01:11 +0100

    Admin module update

---
+++ modular-gaming/user/config/admin.php
@@ -0,0 +1,10 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array (
+	'user' => array(
+		'nav' => array(
+			'index' => array('link' => 'user', 'title' => 'User', 'active' => false),
+			'avatar' => array('link' => 'avatar', 'title' => 'Avatars', 'active' => false),
+		)
+	)
+);
---
+++ modular-gaming/user/config/avatar.php
@@ -0,0 +1,8 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+return array(
+	'size' => array(
+		'width' => 50,
+		'heigth' => 50
+	)
+);
