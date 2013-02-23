a381bbe736fab48ca53e6c5b3922a79156ac4852
Kerstens Maxim <admin@happydemon.org>
2013-02-23 14:27:03 +0100

    Log helper added
    look at the docs for info
    using notification:
    Log::create(param,...)->notify($user,...);

---
+++ modulargaming/core/classes/Log.php
@@ -0,0 +1,85 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+/**
+ * Log class.
+ *
+ * @package    MG/core
+ * @category   Log
+ * @author     Modular Gaming Team
+ * @copyright  (c) 2012-2013 Modular Gaming Team
+ * @license    BSD http://modulargaming.com/license
+ */
+class Log {
+	/**
+	 * Create a new log
+	 *
+	 * @param string     $alias   An identifier to help index logs
+	 * @param string     $type    Module name that's logging (e.g. item, pet,..)
+	 * @param string     $message A general message describing the action
+	 * @param array      $params  Parameters that give insight into the action that has been performed
+	 * @param Model_User $user    The user that did an action(defaults to the logged in user if null)
+	 *
+	 * @return Log
+	 */
+	static public function create($alias, $type, $message, $params = array(), $user = NULL)
+	{
+		if ($user == NULL)
+		{
+			$user = Auth::instance()->get_user();
+		}
+
+		$values = array(
+			'alias'    => $alias,
+			'message'  => $message,
+			'user_id'  => $user->id,
+			'agent'    => Request::user_agent(array('browser', 'platform')),
+			'ip'       => Request::$client_ip,
+			'location' => Request::current()->uri(),
+			'type'     => $type,
+			'params'   => $params,
+		);
+
+		$log = ORM::factory('Log')
+			->values($values)
+			->create();
+
+		return new Log($log);
+	}
+
+	protected $_log = null;
+
+	public function __construct($log) {
+		$this->_log = $log;
+	}
+
+	/**
+	 * Send a notification to a user based on a log.
+	 *
+	 * @param Model_User $user         User instance we'll be notifying
+	 * @param string     $notification A string that can be parsed through __()
+	 * @param array      $param        Params to parse the notification with (combined with $log->params)
+	 * @param string     $type         Type of notification (info, error, success, warning)
+	 *
+	 * @return User_Notification
+	 */
+	public function notify($user, $notification, $param = array(), $type = "info")
+	{
+		$log = $this->_log;
+
+		$notify = Kohana::$config->load('notify.' . $notification);
+
+		$param['username'] = $log->user->username;
+
+		$values = array(
+			'log_id'  => $log->id,
+			'user_id' => $user->id,
+			'title'   => $notify['title'],
+			'message' => __($notify['message'], $log->params + $param),
+			'icon'    => $notify['icon'],
+			'type'    => $type
+		);
+
+		return ORM::factory('User_Notification')
+			->values($values)
+			->create();
+	}
+}
