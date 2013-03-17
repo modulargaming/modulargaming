5b418f6c5c172884056b79721c471f7026cdd407
Hinton <hinton.oscar@gmail.com>
2013-03-17 23:27:21 +0100

    Added basic user guide to forum.

---
+++ modulargaming/forum/config/userguide.php
@@ -0,0 +1,12 @@
+<?php defined('SYSPATH') OR die('No direct access allowed.');
+
+return array (
+	'modules' => array(
+		'forum' => array(
+			'enabled'     => TRUE,
+			'name'        => 'Forum',
+			'description' => 'Lightweight forum system.',
+			'copyright'   => '&copy; 2012–2013 Modular Gaming Team',
+		)
+	)
+);
