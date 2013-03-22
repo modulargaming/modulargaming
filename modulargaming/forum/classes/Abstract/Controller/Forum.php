9c8764058e194b7e45abdb9c6bd92001cccce8eb
Hinton <hinton.oscar@gmail.com>
2013-03-22 20:03:00 +0100

    Added config variable for public forum, #135.

--- modulargaming/forum/classes/Abstract/Controller/Forum.php
+++ modulargaming/forum/classes/Abstract/Controller/Forum.php
@@ -10,11 +10,24 @@
  */
 class Abstract_Controller_Forum extends Abstract_Controller_Frontend {
 
-//	protected $protected = TRUE;
 	protected $protected = FALSE;
+
+	/**
+	 * @var array Forum config array.
+	 */
+	protected $config;
 
 	public function before()
 	{
+		$this->config = Kohana::$config->load('forum');
+
+		// Check if we allow guests to view the forum in config.
+		// TODO: improve this check, database based depending on category?
+		if ($this->protected === FALSE AND $this->config['guest'] === FALSE)
+		{
+			$this->protected = TRUE;
+		}
+
 		parent::before();
 
 		// TODO: Check if user can view forum?
--- modulargaming/forum/config/forum.php
+++ modulargaming/forum/config/forum.php
@@ -1,6 +1,8 @@
 <?php defined('SYSPATH') OR die('No direct access allowed.');
 
 return array (
+	'guest' => TRUE, // Allow guests to view the forum?
+
 	'topic' => array(
 		'pagination' => 5,
 		'ajax' => FALSE,
