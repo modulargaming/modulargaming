cdb0c780765017791a50b1c70d469dfa76869877
Curtis Delicata <curtis@modulargaming.com>
2013-03-25 11:04:05 +0000

    Add profile link to pets #146 #101

--- modulargaming/pet/classes/View/Pet/Index.php
+++ modulargaming/pet/classes/View/Pet/Index.php
@@ -3,4 +3,24 @@
 class View_Pet_Index extends Abstract_View_Pet {
 
 	public $title = 'Your pets';
+
+	public $pets;
+
+	public function pets()
+	{
+		$pets = array();
+		foreach ($this->pets as $pet)
+		{
+			$pets[] = array(
+				'id'  => $pet->id,
+				'src' => URL::base().'assets/img/pets/'.$pet->specie->id.'/'.$pet->colour->image,
+				'href' => Route::url('pet', array('name' => strtolower($pet->name))),
+				'name' => $pet->name,
+				'specie' => $pet->specie->name,
+				'colour' => $pet->colour->name,
+				'username' => $pet->user->username
+			);
+		}
+		return $pets;
+	}
 }
