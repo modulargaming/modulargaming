8ae1b2e18f76a9770b02d44afec83789c03cda9b
Lewis <lwsdlct@gmail.com>
2013-04-20 23:05:01 +0100

    Improved game

--- modulargaming/game/classes/Controller/Game/Rockpaperscissors.php
+++ modulargaming/game/classes/Controller/Game/Rockpaperscissors.php
@@ -5,10 +5,6 @@ class Controller_Game_Rockpaperscissors extends Abstract_Controller_Game {
 
 	public function action_index()
 	{
-
-		$points = Kohana::$config->load('items.points');
-		$initial_points = $points['initial'];
-
 		$this->view = new View_Game_Rockpaperscissors;
 		$game = ORM::factory('Game')
 			->where('game_id', '=', 1)
@@ -28,19 +24,7 @@ class Controller_Game_Rockpaperscissors extends Abstract_Controller_Game {
 					)
 				);
 		}
-		$can_play = true;
-		if ($game->plays >= 5)
-		{
-			if ($game->last_play > time() - 86400)
-			{
-				$can_play = false;
-			}
-			else
-			{
-				$game->plays = 0;
-				$game->save();
-			}
-		}
+		$can_play = $game->can_play();
 		$this->view->can_play = $can_play;
 		if ($this->request->method() == HTTP_Request::POST AND $can_play)
 		{
@@ -49,40 +33,17 @@ class Controller_Game_Rockpaperscissors extends Abstract_Controller_Game {
 				$post = $this->request->post();
 				if (isset($post['collect']) AND $game->winnings)
 				{
-					$this->user->set_property('points', $this->user->get_property('points', $initial_points) + $game->winnings);
-					$this->user->save();
-					$game->winnings = 0;
-					$game->plays ++;
-					$game->last_play = time();
-					$game->save();
+					$game->collect_winnings();
 					Hint::success('You have collected your winnings');
 					$this->redirect(Route::url('game', array('controller' => 'rockpaperscissors')));
 				}
-				if (isset($post['move']) AND ($post['move'] == 'rock' OR $post['move'] == 'paper' OR $post['move'] == 'scissors'))
+				$validation = Validation::factory($post)
+   						 ->rule('move', 'not_empty')
+   						 ->rule('move', 'in_array', array(':value', array('rock', 'paper', 'scissors')));
+
+				if ($validation->check())
 				{
-					$player = $post['move'];
-					$choices = array('rock', 'paper', 'scissors');
-					$npc = $choices[mt_rand(0, 2)];
-					$win = 0;
-					if (($player == 'rock' AND $npc == 'scissors') OR ($player == 'paper' AND $npc == 'rock') OR ($player == 'scissors' AND $npc == 'paper'))
-					{
-						$win = round($game->winnings * 0.25 + 10);
-						$game->winnings = $game->winnings + $win;
-						$game->save();
-					}
-					else if ($player != $npc)
-					{
-						$game->winnings = 0;
-						$game->plays ++;
-						$game->last_play = time();
-						$game->save();
-					}
-					$play = array(
-						'player' => $player,
-						'npc'    => $npc,
-						'win'    => $win,
-						'draw'   => $player == $npc
-					);
+					$play = $this->play_game($post['move'], $game);
 					$this->view->play = $play;
 				}
 			}
@@ -94,4 +55,28 @@ class Controller_Game_Rockpaperscissors extends Abstract_Controller_Game {
 		$this->view->game = $game;
 	}
 
+	private function play_game($choice, $game)
+	{
+		$win = 0;
+    		$choices = array('rock', 'paper', 'scissors');
+		$npc = $choices[mt_rand(0, 2)];
+		if (($choice == 'rock' AND $npc == 'scissors') OR ($choice == 'paper' AND $npc == 'rock') OR ($choice == 'scissors' AND $npc == 'paper'))
+		{
+			$win = round($game->winnings * 0.25 + 10);
+			$game->winnings = $game->winnings + $win;
+		}
+		else if ($choice != $npc)
+		{
+			$game->winnings = 0;
+			$game->plays ++;
+			$game->last_play = time();
+		}
+		$game->save();
+		return array(
+			'player' => $choice,
+			'npc'    => $npc,
+			'win'    => $win,
+			'draw'   => $choice == $npc
+		);
+	}
 }
--- modulargaming/game/classes/Model/Game.php
+++ modulargaming/game/classes/Model/Game.php
@@ -12,6 +12,41 @@ class Model_Game extends ORM
 		'winnings'      => NULL
 	);
 
+	protected $_belongs_to = array(
+		'user' => array(
+			'model' => 'User',
+		)
+	);
+
+	public function can_play()
+	{
+		if ($this->plays >= 5)
+		{
+			if ($this->last_play > time() - Date::DAY)
+			{
+				return FALSE;
+			}
+			else
+			{
+				$this->plays = 0;
+				$this->save();
+			}
+		}
+		return TRUE;
+	}
+
+	public function collect_winnings()
+	{
+		$points = Kohana::$config->load('items.points');
+		$initial_points = $points['initial'];
+		$this->user->set_property('points', $this->user->get_property('points', $initial_points) + $this->winnings);
+		$this->user->save();
+		$this->winnings = 0;
+		$this->plays ++;
+		$this->last_play = time();
+		$this->save();
+	}
+
 	public function create_game($values, $expected)
 	{
 		return $this->values($values, $expected)
