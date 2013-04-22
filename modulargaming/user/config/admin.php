62921bac50c69c84c964b1244144b65ffb2f98e0
Lewis <lwsdlct@gmail.com>
2013-04-22 16:53:07 +0100

    Admin user nav

--- modulargaming/user/config/admin.php
+++ modulargaming/user/config/admin.php
@@ -3,8 +3,9 @@
 return array(
 	'user' => array(
 		'nav' => array(
-			'index'  => array('link' => 'user', 'title' => 'User', 'active' => FALSE),
+			'user'  => array('link' => 'user', 'title' => 'Users', 'active' => FALSE),
 			'avatar' => array('link' => 'avatar', 'title' => 'Avatars', 'active' => FALSE),
+			'role'   => array('link' => 'user/role', 'title' => 'Roles', 'active' => FALSE),
 		)
 	)
 );
