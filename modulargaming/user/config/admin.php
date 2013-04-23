aebb895f82b9957f5ca8d411c4fe9a80e50b6c7f
Curtis Delicata <curtis@modulargaming.com>
2013-04-23 09:42:30 +0100

    Change order of navbar #74

--- modulargaming/user/config/admin.php
+++ modulargaming/user/config/admin.php
@@ -4,8 +4,8 @@ return array(
 	'user' => array(
 		'nav' => array(
 			'user'  => array('link' => 'user', 'title' => 'Users', 'active' => FALSE),
-			'avatar' => array('link' => 'avatar', 'title' => 'Avatars', 'active' => FALSE),
 			'role'   => array('link' => 'user/role', 'title' => 'Roles', 'active' => FALSE),
+			'avatar' => array('link' => 'avatar', 'title' => 'Avatars', 'active' => FALSE),
 		)
 	)
 );
