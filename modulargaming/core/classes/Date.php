44c4946394df197dc7afb195550a8f548c4107a7
Hinton <hinton.oscar@gmail.com>
2013-05-02 17:24:03 +0200

    Started on transparent extension for core.

--- modulargaming/core/classes/Date.php
+++ modulargaming/core/classes/Date.php
@@ -1,38 +1,3 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
-/**
- * Date helper.
- *
- * @package    Modular Gaming
- * @category   Helpers
- * @author     Modular Gaming Team
- * @copyright  (c) 2013 Modular Gaming Team
- * @license    BSD http://modulargaming.com/license
- */
-
-
-class Date extends Kohana_Date {
-
-	/**
-	 * Format timestamp to users format.
-	 *
-	 * @param   integer $timestamp Timestamp to format
-	 * @return  string
-	 */
-	public static function format($timestamp)
-	{
-		$auth = Auth::instance();
-		$format = Kohana::$config->load('date.default_format');
-
-		if ($auth->logged_in())
-		{
-			$user = $auth->get_user();
-			$timezone = $user->timezone->timezone;
-			$offset = Date::offset($timezone);
-			$timestamp = $timestamp + $offset;
-		}
-
-		return strftime($format, $timestamp);
-	}
-
-}
+class Date extends MG_Date {}
