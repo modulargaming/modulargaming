dff9536c2b981c167a08d99394062cb53f547c3f
Curtis Delicata <curtis@modulargaming.com>
2013-05-03 12:48:41 +0100

    Admin transparent classes #173

--- modulargaming/admin/classes/MG/View/Admin/Dashboard/Index.php
+++ modulargaming/admin/classes/MG/View/Admin/Dashboard/Index.php
@@ -1,6 +1,6 @@
 <?php defined('SYSPATH') OR die('No direct script access.');
 
-class View_Admin_Dashboard_Index extends Abstract_View_Admin {
+class MG_View_Admin_Dashboard_Index extends MG_Abstract_View_Admin {
 
 	public $title = 'Admin - Dashboard - Index';
 
