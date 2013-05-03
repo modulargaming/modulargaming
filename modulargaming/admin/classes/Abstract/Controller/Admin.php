21033b245c939dbc226d2583638a919ab9d25e51
Curtis Delicata <curtis@modulargaming.com>
2013-05-03 12:49:39 +0100

    Missing files #173

---
+++ modulargaming/admin/classes/Abstract/Controller/Admin.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Abstract_Controller_Admin extends MG_Abstract_Controller_Admin {} 
---
+++ modulargaming/admin/classes/View/Admin/Dashboard/Index.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class View_Admin_Dashboard_Index extends MG_View_Admin_Dashboard_Index {} 
