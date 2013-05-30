b452baaa8b529b0b16fd8dfa94370e46c1f3e6a9
Curtis Delicata <curtis@modulargaming.com>
2013-05-30 10:26:01 +0100

    Add missing avatar drivers

---
+++ modulargaming/user/classes/Avatar/Gravatar.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Avatar_Gravatar extends MG_Avatar_Gravatar {}
---
+++ modulargaming/user/classes/Avatar/Upload.php
@@ -0,0 +1,3 @@
+<?php defined('SYSPATH') OR die('No direct script access.');
+
+class Avatar_Upload extends MG_Avatar_Upload {}
